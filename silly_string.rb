0709ec5c33aecca74b7c8e6e2132266fbd05d77b
Uma Desai <umadesai38@gmail.com>
2018-07-09 22:34:23 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -3,7 +3,7 @@
 
 your_favorite_word = ""
 
-puts "My favorite word is #{your_favorite_word}"
+puts "My favorite word is "Doggo"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
